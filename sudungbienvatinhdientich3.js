9dde23f0c4cf47c556847e4ef65c5bba14dd5f0e
XuxiLinhmy <linhmy1501@gmail.com>
2025-07-29 22:19:41 +0900

    bai tap 03

---
+++ sudungbienvatinhdientich3.js
@@ -0,0 +1,5 @@
+let width = Number(prompt('Nhập vào chiều dài hình chữ nhật'));
+let height = Number(prompt('Nhập vào chiều cao hình chữ nhật'));
+area = width*height;
+alert(area);
+console.log('Diện tích là:' +width*height);
