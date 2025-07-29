b6d75fb89ff63c391b8a9eb60d55095705433fba
XuxiLinhmy <linhmy1501@gmail.com>
2025-07-29 22:21:27 +0900

    bai tap 06

---
+++ tinhdiemtrungbinh6.js
@@ -0,0 +1,6 @@
+let math = Number(prompt('Mời bạn nhập điểm môn học thứ nhất'));
+let physics = Number(prompt('Mời bạn nhập điểm môn học thứ hai'));
+let chemistry = Number(prompt('Mời bạn nhập điểm môn học thứ 3'));
+let medium = (math + physics + chemistry)/3;
+
+console.log(medium);
